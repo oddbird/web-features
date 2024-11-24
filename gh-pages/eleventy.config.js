98e8d06265e443b9eb36d5fe2ec87d01f64a19f9
James Stuckey Weber <james@oddbird.net>
2024-11-23 22:31:17 -0500

    Add features API

--- gh-pages/eleventy.config.js
+++ gh-pages/eleventy.config.js
@@ -2,6 +2,7 @@ const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
 
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/assets/img");
   eleventyConfig.addPassthroughCopy("./src/assets/fonts");
@@ -13,6 +14,12 @@ module.exports = function (eleventyConfig) {
     linkify: true,
   };
   eleventyConfig.setLibrary("md", markdownIt(mdOpts).use(markdownItAnchor));
+  eleventyConfig.addGlobalData("features", async () => {
+    // Requires build from root- `cd ..; npm run build`.
+    const { features } = await import('../packages/web-features/index.js')
+    return Object.keys(features).map(id=> ({...features[id], id}));
+  });
+
   return {
     dir: {
       input: "src",
